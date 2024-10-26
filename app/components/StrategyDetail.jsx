5b383a1cb5d482a591537bf8750fb0a8a2bdf39c
Mikko Käär <mikko.kaar@gmail.com>
2024-10-26 19:42:31 +0300

    fix back to index so its just the text that will register a click

--- app/components/StrategyDetail.jsx
+++ app/components/StrategyDetail.jsx
@@ -10,7 +10,8 @@ export default function StrategyDetail({ strategy }) {
       </div>
       <p className="text-2xl mb-6 text-gray-300">{strategy.description}</p>
       {strategy.content}
-      <Link to="/" className="text-blue-500 hover:underline mt-4 text-lg block mt-8">Back to Index</Link>
+      
+      <Link to="/" className="text-blue-500 hover:underline mt-4 text-lg inline-block mt-8">Back to Index</Link>
     </div>
   );
 }
